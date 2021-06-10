71fe91f3afa80ca142ad8ce99c63893faed9bbf5
tdppollard <45292464+tdppollard@users.noreply.github.com>
2021-06-10 18:48:32 +0100

    Addition of Wittertainment topic

---
+++ inst/topics/Wittertainment.R
@@ -0,0 +1,32 @@
+# Wittertainment
+
+# for more information visit \href{https://www.denofgeek.com/movies/the-beginner-s-guide-to-wittertainment/}
+
+c(
+  "hello to Jason Isaacs",
+  "love the show Steve",
+  "the church",
+  "LTL and FTE",
+  "the cruise",
+  "code of conduct",
+  "fart gun",
+  "*snigger* bottom",
+  "fruit-based device",
+  "Dougal and the blue cat",
+  "The Exorcist",
+  "The good doctor",
+  "stinky pants wee!",
+  "emergency mail",
+  "everything is going to be alright in the end",
+  "birdsong",
+  "Meg Ryan is a helicopter pilot",
+  "It's not about a shark",
+  "Peter from Germany",
+  "The good lady professor her in doors",
+  "tinkerty tonk and down with the Nazis",
+  "quantum baby!",
+  "Brechtian alienation device",
+  "6 laugh test",
+  "machine for creating empathy",
+  "female pipe smokers"
+)
