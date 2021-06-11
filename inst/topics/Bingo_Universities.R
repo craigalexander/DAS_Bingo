a974c8806d39e9d89263991011853e256ef83968
superchaosxu <54112618+superchaosxu@users.noreply.github.com>
2021-06-11 14:16:51 +0100

    Add this R file.

---
+++ inst/topics/Bingo_Universities.R
@@ -0,0 +1,30 @@
+## Aim : bingo card proposal for the Universities
+## Author : Chao Xu
+c(
+  "University of Oxford",
+  "University of Cambridge",
+  "Imperial College London",
+  "UCL",
+  "The University of Edinburgh",
+  "The University of Manchester",
+  "King's College London",
+  "LSE",
+  "The University of Warwick",
+  "University of Bristol",
+  "University of Glasgow",
+  "University of Southampton",
+  "Durham University",
+  "University of Birmingham",
+  "University of St Andrews",
+  "University of Leeds",
+  "The University of Sheffield",
+  "University of Nottingham",
+  "Queen Mary University of London",
+  "Lancaster University",
+  "Newcastle University",
+  "The University of Exeter",
+  "Cardiff University",
+  "University of York",
+  "University of Bath",
+  "University of Liverpool"
+)
