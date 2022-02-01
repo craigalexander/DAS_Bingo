20d6c430dc75047bdec95d8d744fa5980cb20573
Shahbaz Khan <2598100k@student.gla.ac.uk>
2022-02-01 23:00:35 +0000

    Bingo cards with name of top 20 youtubers by subscribers number

---
+++ inst/topics/youtubers.R
@@ -0,0 +1,31 @@
+#Top 20 youtubers/youtube channels by subscriber count
+#data source - https://socialblade.com/youtube/top/100/mostsubscribed
+#library(robotstxt)
+#paths_allowed("https://socialblade.com/youtube/top/100/mostsubscribed")
+
+c(
+  "T-Series",
+  "Cocomelon - Nursery Rhymes",
+  "Ed Sheeran",
+  "ChuChu TV Nursery Rhymes",
+  "Marshmello",
+  "Dude Perfect",
+  "Zee TV",
+  "Sony SAB",
+  "BANGTANTV",
+  "HYBE LABELS",
+  "Goldmines",
+  "Canal KondZilla",
+  "Ed Sheeran ",
+  "Justin Bieber ",
+  "BLACKPINK",
+  "5-Minute Crafts",
+  "Vlad and Niki",
+  "Zee Music Company",
+  "WWE",
+  "Like Nastya",
+  "MrBeast",
+  "??? Kids Diana Show",
+  "PewDiePie",
+  "SET India"
+)
